08fbac5c253b5df09d72c404007a4d5932a4fc42
xBromx <ferrarit1999@googlemail.com>
2018-02-19 23:07:12 +0100

    test do download the main programm

---
+++ src/Test.java
@@ -0,0 +1,69 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Test
+{
+
+	public Test()
+	{
+
+	}
+
+	public static void main(String[] args) throws MalformedURLException
+	{
+		String sUrl = "https://drive.google.com/file/d/1U7XsvO-v0ZsqVFjK0iaKm-vJ34G43nya/view?usp=sharing";
+
+		URL url = new URL(sUrl);
+
+		File file = new File("Zahlensysteme.jar");
+
+		Test.copyURLToFile(url, file);
+
+	}
+
+	public static void copyURLToFile(URL url, File file)
+	{
+
+		try
+		{
+			InputStream input = url.openStream();
+			if (file.exists())
+			{
+				if (file.isDirectory())
+					throw new IOException("File '" + file + "' is a directory");
+
+				if (!file.canWrite())
+					throw new IOException("File '" + file + "' cannot be written");
+			} else
+			{
+				File parent = file.getParentFile();
+				if ((parent != null) && (!parent.exists()) && (!parent.mkdirs()))
+				{
+					throw new IOException("File '" + file + "' could not be created");
+				}
+			}
+
+			FileOutputStream output = new FileOutputStream(file);
+
+			byte[] buffer = new byte[4096];
+			int n = 0;
+			while (-1 != (n = input.read(buffer)))
+			{
+				output.write(buffer, 0, n);
+			}
+
+			input.close();
+			output.close();
+
+			System.out.println("File '" + file + "' downloaded successfully!");
+		} catch (IOException ioEx)
+		{
+			ioEx.printStackTrace();
+		}
+	}
+
+}
